4a52ec700ad21b8108bccc801fcecfce95dfdd77
Md Al Amin Hossen <87903038+MdRasen@users.noreply.github.com>
2023-09-28 11:52:50 +0600

    Add files via upload

---
+++ Number Counting Animation/script.js
@@ -0,0 +1,19 @@
+// Get allValues using querySelectorAll
+let allValues = document.querySelectorAll(".value");
+
+// Start the forEach loop for displaying the values
+allValues.forEach((singleValue) => {
+  let startValue = 0;
+  let endValue = parseInt(singleValue.getAttribute("data-value"));
+  let duration = Math.floor(2000 / endValue);
+
+  // Counter for increaing the values & display
+  let counter = setInterval(function () {
+    startValue += 1;
+    singleValue.textContent = startValue;
+    // Clearing the interval
+    if (startValue == endValue) {
+      clearInterval(counter);
+    }
+  }, duration);
+});
